f94889b7dc7521dfc3b57ee1e9cc8d901f52fb67
Maaarcocr <marcoconcettorudilosso@gmail.com>
2016-06-18 10:49:59 +0100

    navbar inline bigger buttons

--- src/bundle.js
+++ src/bundle.js
@@ -172,7 +172,8 @@ var NavItem = React.createClass({
       color: this.state.isHover ? 'white' : "#000",
       padding: '8px 0 8px 16px',
       textDecoration: 'none',
-      backgroundColor: this.state.isHover ? '#555' : '#f1f1f1'
+      backgroundColor: this.state.isHover ? '#555' : '#f1f1f1',
+      height: '70px'
     };
     return React.createElement(
       'li',

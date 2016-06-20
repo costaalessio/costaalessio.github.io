7f00ce0564e88786ca86d0f7394072e6421207c3
Maaarcocr <marcoconcettorudilosso@gmail.com>
2016-06-20 14:59:11 +0100

    button padding and margin

--- src/bundle.js
+++ src/bundle.js
@@ -253,7 +253,7 @@ var ToggleButton = React.createClass({
   },
   render: function render() {
     var pad = 10;
-    var marginV = (this.props.height - pad - 28) / 2;
+    var marginV = (parseInt(his.props.height) - pad - 28) / 2;
     var marginO = 30 - pad;
     var padString = pad.toString() + "px";
     var marginString = marginV.toString() + "px " + marginO.toString() + "px";
